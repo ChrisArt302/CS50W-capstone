68486c8d97266550da7cdcbde36d8a2890d1b004
ChrisArt302 <christian73ism@gmail.com>
2023-02-07 20:01:29 -0800

    django key

--- capstone/settings.py
+++ capstone/settings.py
@@ -13,6 +13,11 @@ https://docs.djangoproject.com/en/4.0/ref/settings/
 from pathlib import Path
 import os
 
+# Initialize environment variables
+import environ
+env = environ.Env()
+environ.Env.read_env()
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -21,10 +26,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-2yr+2e62zsvki102%)a2c@n7c)+k!zu3vlcm1pt(=6*&it$z2*'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
@@ -128,11 +133,6 @@ MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
-
-# Initialize environment variables
-import environ
-env = environ.Env()
-environ.Env.read_env()
 
 
 # Enter email configuration here:
